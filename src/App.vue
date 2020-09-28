d99c1fc6bce354775a41e3f716d0d949a371fdfe
Nicolas Sanchez <nisanchezva@unal.edu.co>
2020-09-28 06:42:13 -0500

    Rutas implementadas

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id ="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">|About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -25,4 +19,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+#nav {
+  padding: 30px;
+
+}
+#nav {
+  font-weight: bold;
+  color: #2c3e50;
+
+}
+
+
 </style>
---
+++ src/views/Iniciar_sesion.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="iniciar_sesion">
+        <h1>Aqui estara el inicio de sesion</h1>
+    </div>
+</template>
