2b64fc4e21714f604d690c203a1939db568924a9
jupbonillago <jupbonillago@unal.edu.co>
2020-09-30 20:27:35 -0500

    menu en top de la pagina principal avance

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <div id="app">
-
-    #Menu en el tope de la pagina
-    <div class="top-menu">
-      <img alt="Vue logo" src="./assets/logo.png" class="icon">
-      <div class="navbar">
-        <a href="#Inicio">Inicio</a>
-        <a href="#Categorias">Categorias</a>
-        <a href="#Mis_productos">Mis productos</a>
-        <a href="#Historial">Historial</a>
-        <a href="#Ayuda">Ayuda</a>
-      </div>
+  <div id="app" style="margin-top: 0px;">
+    <div id="topMenu_div" style="position: sticky;">
+      <TopMenu/>
     </div>
-
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopMenu from './components/TopMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopMenu
   }
 }
 </script>
@@ -38,36 +27,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.navbar {
-  background-color: #2c3e50;
-  align-content: end;
-  overflow: hidden;
-}
-
-.navbar a {
-  float: left;
-  color: #dddddd;
-  text-align: center;
-  padding: 15px 20px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.navbar a:hover {
-  background-color: #ddd;
-  color: black;
-}
-.top-menu {
-  background: beige;
-  position: fixed;
-  top: 0;
-  width: 100%;
-}
-
-.icon{
-  height: 50px;
-  position: unset;
-  left: 0px;
-}
-
 </style>
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- Contenedor para el menu -->
+  <b-container class="menu">
+    <!-- Primera fila de items -->
+    <b-row>
+      <b-col>
+
+      </b-col>
+      <b-col>
+        <b-input-group class="sm">
+          <b-form-input></b-form-input>
+          <b-input-group-append>
+            <b-button variant="">Search</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-col>
+      <b-col>
+
+      </b-col>
+
+    </b-row>
+
+    <!-- Segunda fila de items -->
+    <b-row>
+    <b-navbar class="navbar">
+      <b-navbar-nav>
+        <b-nav-item href="#"><span class="fa fa-home"></span>Inicio</b-nav-item>
+
+        <!-- dropdowns de categorias -->
+        <b-nav-item-dropdown text="Categorias" right>
+          <b-dropdown-item href="#">Tecnologia</b-dropdown-item>
+          <b-dropdown-item href="#">Electrodomesticos</b-dropdown-item>
+          <b-dropdown-item href="#">Juguetes</b-dropdown-item>
+          <b-dropdown-item href="#">Alimentos</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item href="#">Mis productos</b-nav-item>
+        <b-nav-item href="#">Historial</b-nav-item>
+        <b-nav-item href="#">Ayuda</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+    </b-row>
+
+
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "TopMenu"
+}
+</script>
+
+<style scoped>
+.navbar {
+  background: #43B0DF;
+  top: 0;
+  width: 100%;
+  left: 0;
+
+}
+.navbar a {
+
+}
+.menu {
+  background: #43B0DF;
+  margin-left: 0px;
+  margin-right: 0px;
+  max-width: inherit;
+}
+</style>
