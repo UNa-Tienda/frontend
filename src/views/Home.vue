113898df531953d22c123a71da9d79bd318d96f6
unknown <ccmorenov@unal.edu.co>
2020-09-30 21:12:37 -0500

    view category and footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,57 @@
 <template>
-  <div class="container-fluid mt-2">
-    <h1>Pagina principal</h1>
-    <div>
-      <router-link :to="{ name: 'register' }">Registrarse</router-link>
-    </div>
-    <div>
-      <router-link :to="{ name: 'iniciar_sesion' }">Iniciar Sesión</router-link>
-    </div>
-
-    <div class="row">
-      <router-view></router-view>
-    </div>
-  </div>
+  
+  <div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
+        
+        <div id="app">
+            <div class="w3-row-padding w3-padding-16 w3-center" >
+                <div class="w3-quarter">
+                  <img src="../assets/lobo.jpg" alt="lobo" style="width:200px;height: 200px;" id="category1">
+                  <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+                  <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                </div>
+                <div class="w3-quarter">
+                  <img src="../assets/zorro.png" alt="zorro" style="width:200px;height: 200px;" id="category2">
+                  <h3>Let Me Tell You About This Steak</h3>
+                  <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                </div>
+                <div class="w3-quarter">
+                  <img src="../assets/hongo.png" alt="hongo" style="width:200px;height: 200px;" id="category3">
+                  <h3>Cherries, interrupted</h3>
+                  <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                  <p>What else?</p>
+                </div>
+                <div class="w3-quarter">
+                  <img src="../assets/pez.jpg" alt="Pez" style="width:200px;height: 200px;" id="category4">
+                  <h3>Once Again, Robust Wine and Vegetable Pasta</h3>
+                  <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                </div>
+              </div>
+              <div class="w3-center w3-padding-32">
+                <div class="w3-bar">
+                  <a href="#" class="w3-bar-item w3-button w3-hover-black">«</a>
+                  <a href="#" class="w3-bar-item w3-black w3-button">1</a>
+                  <a href="#" class="w3-bar-item w3-button w3-hover-black">2</a>
+                  <a href="#" class="w3-bar-item w3-button w3-hover-black">3</a>
+                  <a href="#" class="w3-bar-item w3-button w3-hover-black">4</a>
+                  <a href="#" class="w3-bar-item w3-button w3-hover-black">»</a>
+                </div>
+              </div>
+              <footer class="w3-row-padding w3-padding-32">
+                <div class="w3">
+                  <h3>Contacto</h3>
+                  <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+                </div>
+              </footer>        
+            </div>
+  </div>          
 </template>
 
-<script></script>
+<script>
+
+</script>
 
 <style>
+@import url(https://www.w3schools.com/w3css/4/w3.css);
 h1 {
   text-align: center;
 }
