db061beae1e59c14f4c3ee52346c1ed1f09bc1d0
jupbonillago <jupbonillago@unal.edu.co>
2020-10-01 20:30:05 -0500

    top menu updated

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -3,42 +3,54 @@
   <b-container class="menu">
     <!-- Primera fila de items -->
     <b-row>
-      <b-col>
 
+      <b-col sm="3">
+        <img src="../assets/logo.png" style="width: 15vw; padding-top: 10px" >
       </b-col>
-      <b-col>
-        <b-input-group class="sm">
-          <b-form-input></b-form-input>
+
+      <b-col sm="8">
+        <b-input-group class="sm" style="padding-top: 20px;">
+          <b-form-input placeholder="Buscar productos..."></b-form-input>
           <b-input-group-append>
-            <b-button variant="">Search</b-button>
+            <b-button variant="">Buscar</b-button>
           </b-input-group-append>
         </b-input-group>
       </b-col>
-      <b-col>
 
+      <b-col sm="1">
+        <div>
+          <router-link :to="{ name: 'register' }"><img src="../assets/Shopping_cart.png" style="width: 5vw; padding-top: 10px" ></router-link>
+        </div>
       </b-col>
-
     </b-row>
 
     <!-- Segunda fila de items -->
     <b-row>
-    <b-navbar class="navbar">
-      <b-navbar-nav>
-        <b-nav-item href="#"><span class="fa fa-home"></span>Inicio</b-nav-item>
 
-        <!-- dropdowns de categorias -->
-        <b-nav-item-dropdown text="Categorias" right>
-          <b-dropdown-item href="#">Tecnologia</b-dropdown-item>
-          <b-dropdown-item href="#">Electrodomesticos</b-dropdown-item>
-          <b-dropdown-item href="#">Juguetes</b-dropdown-item>
-          <b-dropdown-item href="#">Alimentos</b-dropdown-item>
-        </b-nav-item-dropdown>
+      <b-col sm="6" offset="1">
 
-        <b-nav-item href="#">Mis productos</b-nav-item>
-        <b-nav-item href="#">Historial</b-nav-item>
-        <b-nav-item href="#">Ayuda</b-nav-item>
-      </b-navbar-nav>
-    </b-navbar>
+        <b-navbar class="navbar">
+          <b-navbar-nav>
+            <b-nav-item>Inicio</b-nav-item>
+
+            <!-- dropdowns de categorias -->
+            <b-nav-item-dropdown text="Categorias" right>
+              <b-dropdown-item href="#">Tecnologia</b-dropdown-item>
+              <b-dropdown-item href="#">Electrodomesticos</b-dropdown-item>
+              <b-dropdown-item href="#">Juguetes</b-dropdown-item>
+              <b-dropdown-item href="#">Alimentos</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item href="#">Mis productos</b-nav-item>
+            <b-nav-item href="#">Historial</b-nav-item>
+            <b-nav-item href="#">Ayuda</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar>
+
+      </b-col>
+      <b-col sm="2">
+      </b-col>
+
     </b-row>
 
 
@@ -56,11 +68,11 @@ export default {
   background: #43B0DF;
   top: 0;
   width: 100%;
-  left: 0;
-
+  align-self: center;
 }
 .navbar a {
-
+  color: azure;
+  height: auto;
 }
 .menu {
   background: #43B0DF;
