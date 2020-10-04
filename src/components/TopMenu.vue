c6081046729109e7e6b097b12c2149df14666496
jupbonillago <jupbonillago@unal.edu.co>
2020-10-04 12:40:19 -0500

    ajustes menu

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -2,13 +2,13 @@
   <!-- Contenedor para el menu -->
   <b-container class="menu" fluid="true">
     <!-- Primera fila de items -->
-    <b-row align-content="">
-
-      <b-col class="col-12 col-sm-2 col-lg-2" align-self="center">
-        <img src="../assets/logo.png" style="width: 100%;padding: 10px">
+    <b-row>
+      <b-col class="col-lg-1" style="padding: 0"></b-col>
+      <b-col class="col-11 col-sm-2" align-self="center" style="padding: 0">
+        <img src="../assets/logo.png" style="width: 100%;padding-top: 10px">
       </b-col>
       
-      <b-col class="col-8 col-sm-7" align-self="center">
+      <b-col class="col-7 col-sm-7" align-self="center">
         <b-input-group class="sm">
           <b-form-input placeholder="Buscar productos..."></b-form-input>
           <b-input-group-append>
@@ -28,17 +28,20 @@
     <!-- Segunda fila de items -->
 
 
-      <b-col class="col-1 col-lg-12">
+      <b-col class="col-2 col-lg-12">
 
         <b-navbar toggleable="lg" class="navbar">
 
           <b-navbar-toggle target="nav-collapse" style="background-color: azure;"></b-navbar-toggle>
 
           <b-collapse id="nav-collapse" is-nav class="test">
-            <b-navbar-nav>
+            <b-navbar-nav style="justify-content: space-between; margin-left: 25%; display: flex;width: 60%;flex-direction: row">
               <router-link :to="{ name: 'Inicio' }" class="menufont">Inicio</router-link>
 
-              <b-nav-item-dropdown text="Categorias" right>
+              <b-nav-item-dropdown right>
+                <template v-slot:button-content>
+                <a style="color: azure!important; padding-top: 0">Categorias</a>
+                </template>
                 <b-dropdown-item href="#">Tecnologia</b-dropdown-item>
                 <b-dropdown-item href="#">Alimentos</b-dropdown-item>
                 <b-dropdown-item href="#">Muebles</b-dropdown-item>
@@ -50,14 +53,14 @@
               <router-link :to="{ name: 'ayuda' }" class="menufont">Ayuda</router-link>
             </b-navbar-nav>
 
-            <b-navbar-nav>
-            <router-link :to="{ name: 'register' }" class="menufont"
-                         style="border: 1px solid #2c3e50; padding: 11px; border-radius: 16px; background-color: #2c3e50">
+            <b-navbar-nav class="ml-auto">
+            <router-link :to="{ name: 'register' }"
+                         style="color: azure;border: 1px solid #2c3e50; padding: 11px; margin-right: 5%; border-radius: 16px; background-color: #2c3e50">
               Registarse
             </router-link>
 
-            <router-link :to="{ name: 'iniciar_sesion' }" class="menufont"
-                         style="border: 1px solid #2c3e50; padding: 11px; border-radius: 16px; background-color: #2c3e50">
+            <router-link :to="{ name: 'iniciar_sesion' }"
+                         style="color: azure;border: 1px solid #2c3e50; padding: 11px; border-radius: 16px; background-color: #42b983">
               Ingresar
             </router-link>
             </b-navbar-nav>
@@ -88,7 +91,8 @@ export default {
 
 .menufont {
   color: azure !important;
-  width: 13vw;
+  padding-left: 5vw;
+  padding-right: 5vw;
 }
 
 .menu {
