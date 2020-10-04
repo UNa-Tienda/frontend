b4e7dd1704246028e9df605989e05a0a4ecadb66
jupbonillago <jupbonillago@unal.edu.co>
2020-10-04 16:59:08 -0500

    cambios y ajustes finales

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -1,14 +1,18 @@
 <template>
   <!-- Contenedor para el menu -->
-  <b-container class="menu">
+  <b-container class="menu" >
+
+    <b-row>
+      <b-col class="col-12 col-sm-2" align-self="center">
+        <router-link :to="{ name: 'home' }"><img src="../assets/logo.png" style="width: 100%;padding-top: 10px"></router-link>
+      </b-col>
+
+    <b-col>
 
     <b-row>
       <!-- Primera fila de items -->
-      <b-col class="col-12 col-sm-2" align-self="center">
-        <img src="../assets/logo.png" style="width: 100%;padding-top: 10px">
-      </b-col>
       
-      <b-col class="col-8 col-sm-7" align-self="center">
+      <b-col class="col-8 col-sm-9" align-self="center">
         <b-input-group class="sm">
           <b-form-input placeholder="Buscar productos..."></b-form-input>
           <b-input-group-append>
@@ -19,8 +23,7 @@
 
       <b-col class="col-1 col-sm-1" align-self="center">
         <div>
-          <router-link :to="{ name: 'Shopping_cart' }"><img src="../assets/Shopping_cart.png"
-                                                            style="width: 100%;max-width: 80px; min-width: 25px"></router-link>
+          <router-link :to="{ name: 'shopping_cart' }"><img src="../assets/shopping-cart.png" style="width: 100%;max-width: 80px; min-width: 25px"></router-link>
         </div>
       </b-col>
 
@@ -28,14 +31,14 @@
     <!-- Segunda fila de items -->
 
 
-      <b-col class="col-3 col-sm-1 col-lg-12">
+      <b-col class="col-lg-12">
 
         <b-navbar toggleable="lg" class="navbar">
 
-          <b-navbar-toggle target="nav-collapse" style="background-color: azure;" ></b-navbar-toggle>
+          <b-navbar-toggle target="nav-collapse" style="background-color: azure; width: 30px; padding: 1px" ></b-navbar-toggle>
 
-          <b-collapse id="nav-collapse" is-nav >
-            <b-navbar-nav style="justify-content: space-between; margin-left: 16%; display: flex;width: 60%;flex-direction: row">
+          <b-collapse id="nav-collapse" is-nav class="show">
+            <b-navbar-nav class="right-navbar">
 
               <b-nav-item>
               <router-link :to="{ name: 'home' }" class="menufont">Inicio</router-link>
@@ -65,7 +68,7 @@
 
             </b-navbar-nav>
 
-            <b-navbar-nav class="ml-auto" style="justify-content: space-between; display: flex; flex-direction: row; margin-right: 3vw; width: 17%; max-width: 200px">
+            <b-navbar-nav class="ml-auto left-navbar">
               <b-nav-item style="border: 1px; border-radius: 16px; background-color: #43B0DF">
               <router-link :to="{ name: 'register' }" class="menufont">
                 Registarse
@@ -87,7 +90,8 @@
       </b-col>
 
     </b-row>
-
+    </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -115,6 +119,17 @@ export default {
   margin-right: 0;
   max-width: inherit;
   width: border-box;
+}
+
+@media (min-width: 992px) {
+
+  .left-navbar {
+    justify-content: space-evenly; display: flex; flex-direction: row; margin-right: 12vw; max-width: 200px; width: 17%;
+  }
+
+  .right-navbar {
+    justify-content: space-between; margin-left: 1%; display: flex;width: 60%;flex-direction: row;
+  }
 }
 
 
