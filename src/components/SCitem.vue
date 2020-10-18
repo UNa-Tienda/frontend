47fefb50e03953e174478b432419801bc41e105f
jupbonillago <jupbonillago@unal.edu.co>
2020-10-18 13:24:50 -0500

    vista de carrito de compras finalizada

---
+++ src/components/SCitem.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="border rounded-lg">
+    <div class="row">
+      <div class="col-3" style="align-self: center">
+        <router-link :to="{ name: 'home' }"><img src="../assets/radio.png" style="width: 100%; max-width: 150px"></router-link>
+      </div>
+      <div class="col-6" style="align-self: center">
+        <router-link :to="{ name: 'home' }" style="text-decoration: none!important;"><h3 style="color: white!important;">{{item.title}}</h3></router-link>
+        <h4>precio: ${{item.price}}</h4>
+        <h4>{{item.quantity}} unidades</h4>
+      </div>
+      <div class="col-3" style="align-self: center">
+        <b-button variant="danger">Quitar</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SCitem",
+  props: ["item"]
+}
+</script>
+
+<style scoped>
+.border{
+  border: 1px solid lightskyblue!important;
+  padding: 15px;
+  background-color: lightskyblue;
+}
+h4{
+  color: #012433;
+}
+</style>
--- src/views/Shopping_cart.vue
+++ src/views/Shopping_cart.vue
@@ -1,19 +1,26 @@
 <template>
-<div class="container-fluid">
+<div class="container-fluid" style="padding: 0px">
 
   <div
       id="topMenu_div"
       class="position-sticky"
-      style="position: relative; z-index: 1000"
+      style="position: relative; z-index: 1000;"
   >
     <TopMenu />
   </div>
 
   <div class="main-container">
-    <div class="col-xs-12 col-sm-8 col-md-6 border border-primary rounded-lg">
+    <div class="col-xs-12 col-sm-8 col-md-6 border rounded-lg" style="background-color: azure">
       <h3 class="col-12 text-center text-primary mt-3 mb-3">Mi carrito de Compras</h3>
 
-      
+      <SC_ItemList style="margin-bottom: 20px" v-bind:items="SCitems"/>
+
+      <h4>Total a pagar: $</h4>
+
+      <b-button-group style="margin-bottom: 20px">
+        <b-button style="margin-right: 10px">Cancelar</b-button>
+        <b-button variant="success">Confirmar compra</b-button>
+      </b-button-group>
     </div>
   </div>
 
@@ -26,12 +33,38 @@
 <script>
 import TopMenu from "@/components/TopMenu";
 import Footer from "@/components/Footer";
+import SC_ItemList from "@/components/SC_ItemList";
 
 export default {
   components: {
     TopMenu,
     Footer,
+    SC_ItemList
   },
+  data() {
+    return {
+      SCitems: [
+          {
+            id: 1,
+            title: "primer item radio de baterias marca XYZ",
+            quantity: 1,
+            price: 100
+          },
+        {
+          id: 2,
+          title: "segundo item",
+          quantity: 10,
+          price: 135000
+        },
+        {
+          id: 3,
+          title: "tercer item",
+          quantity: 6,
+          price: 70000
+        }
+      ]
+    }
+  }
 }
 
 </script>
@@ -44,5 +77,9 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.border{
+  border: 1px solid #054c6b!important;
 }
 </style>
