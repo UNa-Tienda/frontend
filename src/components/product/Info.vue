7c0e2029d09a0a8b76a3efd57987ff7790630c77
Adrian-ACI <aacanacuani@unal.edu.co>
2020-10-30 18:57:46 -0500

    consumo API en vista producto

--- src/components/product/Info.vue
+++ src/components/product/Info.vue
@@ -1,54 +1,100 @@
 <template>
-  <div>
-    <div class="infogeneral text-left m-3">
-      <h3 class="mt-2 mb-4">
-        Golty Balón Futbol Dualtech Profesional - T659637 N° 5
-      </h3>
-      <p class="description text-justify mb-3">
-        Balón Golty elaborado en poliuretano, superficie de juego Grama
-        Artificial, Grama Natural, Tierra, arenilla. Diseño moderno, estampado
-        que contrasta y detalles alusivos a la marca. Construcción laminado y
-        cámara de butilo para mayor retención de aire y proporcionando un gran
-        rebote. El balón se envía inflado con la calibración recomendada de
-        fábrica,
-      </p>
-      <h5 class="text-dark">Categoria: Deportes</h5>
-      <br />
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col class="col-xs-12 col-sm-6 col-md-6 col-lg-7 cuadro1 mt-3">
+        <div>
+          <img v-bind:src="image" v-bind:alt="image" class="imageProduct" />
+        </div>
+      </b-col>
+      <b-col class="col-xs-12 col-sm-6 col-md-6 col-lg-5 cuadro2 mt-3">
+        <div>
+          <div class="infogeneral text-left m-3">
+            <h3 class="mt-2 mb-4">
+              {{ product_name }}
+            </h3>
+            <p class="description text-justify mb-3">
+              {{ description }}
+            </p>
+            <h5 class="text-dark">Categoria: {{ category }}</h5>
+            <br />
 
-      <h2>$94.900</h2>
-      <br />
-      <h5 class="text-primary">Stock disponibles: 7 unidades</h5>
+            <h2>Precio: ${{ price }}</h2>
+            <br />
+            <h5 class="text-primary">Stock disponibles: {{ stock }}</h5>
 
-      <div class="cantidad mt-3 mb-2">
-        <b-row>
-          <b-col cols="xs-12 sm-12 auto" md="6">
-            <h4>Cantidad :</h4>
-          </b-col>
-          <b-col cols="xs-12 sm-12 auto" md="6">
-            <b-form-spinbutton
-              class="col-12"
-              id="stock"
-              v-model="cantidad"
-              min="1"
-              max="100"
-            ></b-form-spinbutton>
-          </b-col>
-        </b-row>
-        <div class="mt-5 mb-4 text-success">Llevas {{ cantidad }} Unidades</div>
-      </div>
-      <div>
-        <b-button block variant="danger">Añadir al Carrito</b-button>
-      </div>
-    </div>
-  </div>
+            <div class="cantidad mt-3 mb-2">
+              <b-row>
+                <b-col cols="xs-12 sm-12 auto" md="6">
+                  <h4>Cantidad :</h4>
+                </b-col>
+                <b-col cols="xs-12 sm-12 auto" md="6">
+                  <b-form-spinbutton
+                    class="col-12"
+                    id="stock"
+                    v-model="cantidad"
+                    min="1"
+                    max="10"
+                  ></b-form-spinbutton>
+                </b-col>
+              </b-row>
+              <div class="mt-5 mb-4 text-success">
+                Llevas {{ cantidad }} Unidades
+              </div>
+            </div>
+            <div>
+              <b-button block variant="danger">Añadir al Carrito</b-button>
+            </div>
+          </div>
+          <div><Vendedor /></div>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import axios from "axios";
+import Vendedor from "./Vendedor";
 export default {
+  name: "Info",
   data() {
     return {
-      cantidad: "",
+      product_name: "",
+      image: null,
+      description: "",
+      total_review: 1,
+      price: 10,
+      stock: 1,
+      cantidad: 1,
+      category_id: null,
+      title: "",
     };
+  },
+  beforeCreate() {
+    const postPath = "/api/post/";
+    const product_id = 1;
+
+    axios
+      .get(this.$store.state.backURL + postPath + product_id)
+      .then((response) => {
+        if (response.status !== 200) {
+          alert("Error en la peticion");
+        } else {
+          this.description = response.data.description;
+          this.stock = response.data.stock;
+          this.price = response.data.price;
+          this.category = response.data.category_id.name;
+          this.product_name = response.data.product_name;
+          this.image = response.data.image;
+        }
+      })
+      .catch((response) => {
+        console.log(response.status);
+        alert("No es posible conectar con el backend en este momento");
+      });
+  },
+  components: {
+    Vendedor,
   },
 };
 </script>
@@ -56,5 +102,13 @@ export default {
 <style>
 .cantidad {
   align-content: center;
+}
+.imageProduct {
+  margin-bottom: 20px;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 100%;
+  height: auto;
 }
 </style>
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,177 +1,183 @@
 <template>
-    <div>
-        <div
-                id="topMenu_div"
-                class="position-sticky"
-                style="position: relative; z-index: 1000"
-                >
-                <TopMenu />
-        </div>
-        <div class="container-fluid containerExternoPost">
-          <div class="infoPost">
-            <b-form
-            @submit="createPost"
-            class="datosPost col-xs-12 col-sm-11 col-md-11 col-lg-11"
-            >
-                <b-row >
-                  <h1 class="col-12 text-center text-primary mt-5 mb-5">Publicar Producto</h1>
-                </b-row>
-                <b-row class="mt-5">
-                  <b-col cols="auto" md="6" class="mb-5" >
-                    <div class="imgButton">
-                      <b-form-file v-model="image" class="mt-3" plain></b-form-file>
-                    </div>
-                  </b-col>
-                  <b-col cols=" xs-12 sm-12 auto" md="6" >
-
-                    <b-form-group
-                      class="form-group col-12 mb-4"
-                    >
-                      <b-form-input
-                        id="title"
-                        v-model="title"
-                        type="text"
-                        required
-                        placeholder="Titulo del post"
-                      ></b-form-input>
-                    </b-form-group>
-                    <b-form-group
-                      class="form-group col-12 mb-4"
-
-                    >
-                      <b-form-input
-                        id="product_name"
-                        v-model="product_name"
-                        type="text"
-                        required
-                        placeholder="Nombre del producto"
-                      ></b-form-input>
-                    </b-form-group>
-                    <b-form-group
-                      class="form-group col-12 mb-4"
-                    >
-                      <b-form-input
-                        id="description"
-                        v-model="description"
-                        type="text"
-                        required
-                        placeholder="Descripción"
-                      ></b-form-input>
-                    </b-form-group>
-                    <b-row>
-                      <b-col cols="xs-12 sm-12 auto" md="5" >
-                        Disponibles :  
-                      </b-col>
-                      <b-col cols="xs-12 sm-12 auto" md="6" >
-                        <b-form-spinbutton  class="col-12" id="stock" v-model="stock" min="1" max="100"></b-form-spinbutton>
-                      </b-col>
-                    </b-row>
-                    <br />
-                    <b-form-group
-                      class="form-group col-12 mb-4"
-                    >
-                      <b-form-input
-                        id="price"
-                        v-model="price"
-                        type="number"
-                        required
-                        placeholder="Precio"
-                      ></b-form-input>
-                    </b-form-group>
-                    <b-form-select class="form-group col-12 mb-4" v-model="category" required>
-                      <option value="" disabled selected>-- Seleccione una categoria --</option>
-                      <option v-for="category in categories" :key="category.id" :value="category.id">{{category.name}}</option>
-                    </b-form-select>
-                  </b-col>
-                </b-row>
-                <div class="text-center" align-v="center">
-                  <b-button class ="col-12 mt-2" size="lg" type="sumbit" variant="primary"
-                  >Publicar post
-                  </b-button>
-                </div>
-            </b-form>
-          </div>
-        </div>
-
+  <div>
+    <div
+      id="topMenu_div"
+      class="position-sticky"
+      style="position: relative; z-index: 1000"
+    >
+      <TopMenu />
     </div>
+    <div class="container-fluid containerExternoPost">
+      <div class="infoPost">
+        <b-form
+          @submit="createPost"
+          class="datosPost col-xs-12 col-sm-11 col-md-11 col-lg-11"
+        >
+          <b-row>
+            <h1 class="col-12 text-center text-primary mt-5 mb-5">
+              Publicar Producto
+            </h1>
+          </b-row>
+          <b-row class="mt-5">
+            <b-col cols="auto" md="6" class="mb-5">
+              <div class="imgButton">
+                <b-form-file v-model="image" class="mt-3" plain></b-form-file>
+              </div>
+            </b-col>
+            <b-col cols=" xs-12 sm-12 auto" md="6">
+              <b-form-group class="form-group col-12 mb-4">
+                <b-form-input
+                  id="title"
+                  v-model="title"
+                  type="text"
+                  required
+                  placeholder="Titulo del post"
+                ></b-form-input>
+              </b-form-group>
+              <b-form-group class="form-group col-12 mb-4">
+                <b-form-input
+                  id="product_name"
+                  v-model="product_name"
+                  type="text"
+                  required
+                  placeholder="Nombre del producto"
+                ></b-form-input>
+              </b-form-group>
+              <b-form-group class="form-group col-12 mb-4">
+                <b-form-input
+                  id="description"
+                  v-model="description"
+                  type="text"
+                  required
+                  placeholder="Descripción"
+                ></b-form-input>
+              </b-form-group>
+              <b-row>
+                <b-col cols="xs-12 sm-12 auto" md="5">
+                  Disponibles :
+                </b-col>
+                <b-col cols="xs-12 sm-12 auto" md="6">
+                  <b-form-spinbutton
+                    class="col-12"
+                    id="stock"
+                    v-model="stock"
+                    min="1"
+                    max="100"
+                  ></b-form-spinbutton>
+                </b-col>
+              </b-row>
+              <br />
+              <b-form-group class="form-group col-12 mb-4">
+                <b-form-input
+                  id="price"
+                  v-model="price"
+                  type="number"
+                  required
+                  placeholder="Precio"
+                ></b-form-input>
+              </b-form-group>
+              <b-form-select
+                class="form-group col-12 mb-4"
+                v-model="category"
+                required
+              >
+                <option value="" disabled selected
+                  >-- Seleccione una categoria --</option
+                >
+                <option
+                  v-for="category in categories"
+                  :key="category.id"
+                  :value="category.id"
+                  >{{ category.name }}</option
+                >
+              </b-form-select>
+            </b-col>
+          </b-row>
+          <div class="text-center" align-v="center">
+            <b-button
+              class="col-12 mt-2"
+              size="lg"
+              type="sumbit"
+              variant="primary"
+              >Publicar post
+            </b-button>
+          </div>
+        </b-form>
+      </div>
+    </div>
+  </div>
 </template>
-
 
 <script>
 import TopMenu from "../components/TopMenu.vue";
-import axios from 'axios'
+import axios from "axios";
 
-  const path = '/api/post/add/';
+const path = "/api/post/add/";
 
-  export default {
-    name: "Post.vue",
-    data( ){
-      return{
-        image: null,
-        category_id : '',
-        title: '',
-        product_name: '',
-        description: '',
-        stock: 1,
-        price: '',
-        category: '',
-        categories: [],
-        response: null,
-        
-      }
-    },
-    beforeCreate( ){
-      const categoryPath = '/categories';
+export default {
+  name: "Post.vue",
+  data() {
+    return {
+      image: null,
+      category_id: "",
+      title: "",
+      product_name: "",
+      description: "",
+      stock: 1,
+      price: "",
+      category: "",
+      categories: [],
+      response: null,
+    };
+  },
+  beforeCreate() {
+    const categoryPath = "/categories";
+    axios
+      .get(this.$store.state.backURL + categoryPath)
+      .then((response) => {
+        if (response.status !== 200) {
+          alert("Error en la petición. Intente nuevamente");
+        } else {
+          this.categories = response.data;
+        }
+      })
+      .catch((response) => {
+        alert("No es posible conectar con el backend en este momento");
+        return response;
+      });
+  },
+
+  methods: {
+    createPost(event) {
       axios
-        .get( this.$store.state.backURL + categoryPath )
-        .then( response => {
-          if( response.status !== 200 ){
-            alert( "Error en la petición. Intente nuevamente" )
-          }else{
-            this.categorys = response.data;
+        .post(this.$store.state.backURL + path + this.categories, {
+          image: "Nada",
+          title: this.title.trim(),
+          product_name: this.product_name.trim(),
+          description: this.description.trim(),
+          price: this.price.trim(),
+          stock: this.stock,
+        })
+        .then((response) => {
+          if (response.status !== 201) {
+            alert("Error en la creación del post");
+          } else {
+            alert("Post creado correctamente");
           }
-        }).catch( response => {
-          alert( "No es posible conectar con el backend en este momento" );
-          return response;
+        })
+        .catch((error) => {
+          if (error.response.status === 400) {
+            alert("Un error ocurrio ");
+          } else {
+            alert("Error en la aplicación");
+          }
         });
+      event.preventDefault();
+      return true;
     },
-
-    methods:{
-      createPost( event ){
-        axios
-          .post( this.$store.state.backURL + path + this.categories,
-            {
-    
-              image: "Nada",
-              title: this.title.trim( ),
-              product_name: this.product_name.trim( ),
-              description: this.description.trim( ),
-              price: this.price.trim(),
-              stock: this.stock,
-              
-
-              },
-              
-            
-          ).then( response => {
-            if( response.status !== 201 ){
-              alert( "Error en la creación del post" )
-            }else{
-              alert( "Post creado correctamente" )
-            }
-          }).catch( error =>{
-            if( error.response.status === 400 ){
-              alert( "Un error ocurrio ");
-            }else{
-              alert( "Error en la aplicación" );
-            }
-          });
-        event.preventDefault( );
-        return true;
-      }
-    },
+  },
   components: {
-    TopMenu
+    TopMenu,
   },
 };
 </script>
